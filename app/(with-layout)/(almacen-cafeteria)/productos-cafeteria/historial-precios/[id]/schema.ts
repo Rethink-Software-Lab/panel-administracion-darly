310542e8506c7e531db763ebb7bb27638f25525e
Esteban Daniel Rodríguez Martín <71105277+teby00@users.noreply.github.com>
2025-11-27 05:18:23 -0500

    arreglado problema al retornar saldo tras eliminar ventas

--- app/(with-layout)/(almacen-cafeteria)/productos-cafeteria/historial-precios/[id]/schema.ts
+++ app/(with-layout)/(almacen-cafeteria)/productos-cafeteria/historial-precios/[id]/schema.ts
@@ -1,7 +1,7 @@
-import { enum_, minLength, object, pipe, string } from 'valibot';
-import { TipoPrecio } from './types';
+import { enum_, nonEmpty, object, pipe, string } from "valibot";
+import { TipoPrecio } from "./types";
 
 export const HistorialPreciosSchema = object({
-  tipo: enum_(TipoPrecio, 'Tipo de precio requerido'),
-  precio: pipe(string(), minLength(0, 'El precio debe ser mayor a 0')),
+  tipo: enum_(TipoPrecio, "Tipo de precio requerido"),
+  precio: pipe(string(), nonEmpty("El precio debe ser mayor a 0")),
 });
