68fb07c723691b63288803a930d7c4bb10d5e1d8
Esteban Daniel Rodríguez Martín <71105277+teby00@users.noreply.github.com>
2025-12-26 00:48:43 -0500

    agregando reporte de area venta

---
+++ app/(with-layout)/areas-de-venta/[id]/reporte/page.tsx
@@ -0,0 +1,91 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getReporteVentasAreaVenta } from "./services";
+
+export default async function areaVentaReporte(
+  props: PageProps<"/areas-de-venta/[id]/reporte">
+) {
+  const paramId = (await props.params).id;
+  const areaVentaId = parseInt(paramId);
+
+  if (isNaN(areaVentaId)) return;
+
+  const { data, error } = await getReporteVentasAreaVenta(areaVentaId);
+
+  if (!data) return;
+
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between px-2">
+        <h2 className="text-xl font-semibold">Tu cobro</h2>
+        <span className="font-semibold">
+          {Intl.NumberFormat("es-ES", {
+            style: "currency",
+            currency: "CUP",
+            currencyDisplay: "code",
+          }).format(data?.pagoTrabajador)}
+        </span>
+      </div>
+      <div className="bg-white rounded border px-2">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>Total</TableCell>
+              <TableCell className="text-end">
+                {Intl.NumberFormat("es-ES", {
+                  style: "currency",
+                  currency: "CUP",
+                  currencyDisplay: "code",
+                }).format(data?.total)}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Efectivo</TableCell>
+              <TableCell className="text-end">
+                {Intl.NumberFormat("es-ES", {
+                  style: "currency",
+                  currency: "CUP",
+                  currencyDisplay: "code",
+                }).format(data?.efectivo)}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Transferencia</TableCell>
+              <TableCell className="text-end">
+                {Intl.NumberFormat("es-ES", {
+                  style: "currency",
+                  currency: "CUP",
+                  currencyDisplay: "code",
+                }).format(data?.transferencia)}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+      <div className="bg-white rounded border px-2">
+        <Table>
+          <TableHeader>
+            <TableHead>Producto</TableHead>
+            <TableHead className="text-end">Cantidad</TableHead>
+          </TableHeader>
+          <TableBody>
+            {data?.productos.map((p) => (
+              <TableRow key={p.id}>
+                <TableCell>{p.descripcion}</TableCell>
+                <TableCell className="text-end">
+                  {Intl.NumberFormat().format(p.cantidad)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
---
+++ app/(with-layout)/areas-de-venta/[id]/reporte/services.ts
@@ -0,0 +1,117 @@
+import {
+  TipoCuenta,
+  TipoTransferencia,
+} from "@/app/(with-layout)/finanzas/types";
+import { db } from "@/db/initial";
+import {
+  inventarioCuentas,
+  inventarioProducto,
+  inventarioProductoinfo,
+  inventarioTransacciones,
+  inventarioVentas,
+} from "@/db/schema";
+import { and, count, eq, gte } from "drizzle-orm";
+
+export async function getReporteVentasAreaVenta(areaVentaId: number) {
+  const fechaInicioDia = new Date();
+  fechaInicioDia.setHours(0, 0, 0, 0);
+  try {
+    const p = await db
+      .select({
+        id: inventarioProductoinfo.id,
+        descripcion: inventarioProductoinfo.descripcion,
+        cantidad: count(inventarioProducto),
+      })
+      .from(inventarioProductoinfo)
+      .innerJoin(
+        inventarioProducto,
+        eq(inventarioProducto.infoId, inventarioProductoinfo.id)
+      )
+      .innerJoin(
+        inventarioVentas,
+        eq(inventarioProducto.ventaId, inventarioVentas.id)
+      )
+      .where(
+        and(
+          gte(inventarioVentas.createdAt, fechaInicioDia.toISOString()),
+          eq(inventarioProducto.areaVentaId, areaVentaId)
+        )
+      )
+      .groupBy(inventarioProductoinfo.id);
+
+    const transacciones = await db
+      .select()
+      .from(inventarioTransacciones)
+      .innerJoin(
+        inventarioVentas,
+        eq(inventarioTransacciones.ventaId, inventarioVentas.id)
+      )
+      .innerJoin(
+        inventarioCuentas,
+        eq(inventarioTransacciones.cuentaId, inventarioCuentas.id)
+      )
+      .where(
+        and(
+          gte(inventarioVentas.createdAt, fechaInicioDia.toISOString()),
+          eq(inventarioVentas.areaVentaId, areaVentaId)
+        )
+      );
+
+    const resumen = transacciones.reduce(
+      (acc, row) => {
+        const cantidad = parseFloat(row.inventario_transacciones.cantidad);
+        const tipoTransaccion = row.inventario_transacciones
+          .tipo as TipoTransferencia;
+        const tipoCuenta = row.inventario_cuentas.tipo;
+
+        if (
+          tipoCuenta === TipoCuenta.BANCARIA &&
+          [TipoTransferencia.VENTA, TipoTransferencia.INGRESO].includes(
+            tipoTransaccion
+          )
+        ) {
+          acc.transferencia += cantidad;
+        }
+
+        if (
+          tipoCuenta === TipoCuenta.EFECTIVO &&
+          [TipoTransferencia.VENTA, TipoTransferencia.INGRESO].includes(
+            tipoTransaccion
+          )
+        ) {
+          acc.efectivo += cantidad;
+        }
+
+        if (
+          tipoCuenta === TipoCuenta.EFECTIVO &&
+          [
+            TipoTransferencia.PAGO_TRABAJADOR,
+            TipoTransferencia.EGRESO,
+          ].includes(tipoTransaccion)
+        ) {
+          acc.pagos += cantidad;
+        }
+
+        return acc;
+      },
+      { transferencia: 0, efectivo: 0, pagos: 0 }
+    );
+
+    return {
+      data: {
+        productos: p,
+        total: resumen.efectivo + resumen.transferencia,
+        efectivo: resumen.efectivo,
+        transferencia: resumen.transferencia,
+        pagoTrabajador: resumen.pagos,
+      },
+      error: null,
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      data: null,
+      error: "Error al obtenel el reporte.",
+    };
+  }
+}
